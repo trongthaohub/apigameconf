56a99268bb0a11f83e3a49249adee58a021796d8
trongthaohub <nttgameryb@gmail.com>
2025-08-10 08:40:33 +0700

    Create Responsive.js

---
+++ Responsive.js
@@ -0,0 +1,195 @@
+{
+    "timestamp": "2025-08-10T08:33:21.721612386",
+    "traceId": "31ccd670-4871-4b63-9df9-3bd19add3cef",
+    "code": "success",
+    "message": "0000",
+    "data": {
+        "id": null,
+        "name": "Tập sự gieo hạt",
+        "code": "tap_su_gieo_hat",
+        "icon": "https://file.gamee.vn/1733883711457_Tập sự  gieo hạt.png",
+        "yourScore": 270,
+        "rankScore": 0,
+        "rankConfigs": [
+            {
+                "id": 14,
+                "name": "Tập sự gieo hạt",
+                "code": "tap_su_gieo_hat",
+                "icon": "https://file.gamee.vn/1733883711457_Tập sự  gieo hạt.png",
+                "fromLevel": 0,
+                "toLevel": 2,
+                "requiredTotalExp": 0,
+                "rankAwards": [],
+                "receivingStatus": 1
+            },
+            {
+                "id": 15,
+                "name": "Nông phu lấm lem",
+                "code": "nong_phu_lam_lem",
+                "icon": "https://file.gamee.vn/1733883723039_Nông phu lấm lem.png",
+                "fromLevel": 3,
+                "toLevel": 6,
+                "requiredTotalExp": 3400,
+                "rankAwards": [
+                    {
+                        "id": 51,
+                        "awardType": {
+                            "id": 4,
+                            "name": "Khung avatar"
+                        },
+                        "quantity": 1,
+                        "image": "https://file.gamee.vn/1734924793704_avatar_2.png"
+                    }
+                ],
+                "receivingStatus": 0
+            },
+            {
+                "id": 26,
+                "name": "Nông gia khéo léo",
+                "code": "nong_gia_kheo_leo",
+                "icon": "https://file.gamee.vn/bd4bfbab0-Nông gia khéo léo.png",
+                "fromLevel": 7,
+                "toLevel": 10,
+                "requiredTotalExp": 11300,
+                "rankAwards": [
+                    {
+                        "id": 40,
+                        "awardType": {
+                            "id": 1,
+                            "name": "XP mỗi ngày"
+                        },
+                        "quantity": 50,
+                        "image": "https://file.gamee.vn/1733899218180_exp.f.svg"
+                    },
+                    {
+                        "id": 54,
+                        "awardType": {
+                            "id": 4,
+                            "name": "Khung avatar"
+                        },
+                        "quantity": 1,
+                        "image": "https://file.gamee.vn/1734924876608_avatar_4.png"
+                    }
+                ],
+                "receivingStatus": 0
+            },
+            {
+                "id": 27,
+                "name": "Bậc thầy đồng áng",
+                "code": "bac_thay_dong_ang",
+                "icon": "https://file.gamee.vn/da5965599-rank_4.png",
+                "fromLevel": 11,
+                "toLevel": 15,
+                "requiredTotalExp": 37300,
+                "rankAwards": [
+                    {
+                        "id": 42,
+                        "awardType": {
+                            "id": 1,
+                            "name": "XP mỗi ngày"
+                        },
+                        "quantity": 100,
+                        "image": "https://file.gamee.vn/1733899196095_exp.f.svg"
+                    },
+                    {
+                        "id": 43,
+                        "awardType": {
+                            "id": 2,
+                            "name": "Ting Ting"
+                        },
+                        "quantity": 2000,
+                        "image": "https://file.gamee.vn/1734170262009_coin.png"
+                    },
+                    {
+                        "id": 44,
+                        "awardType": {
+                            "id": 3,
+                            "name": "Giảm giá"
+                        },
+                        "quantity": 5,
+                        "image": "https://file.gamee.vn/1733890016264_giamgia2.jpg"
+                    }
+                ],
+                "receivingStatus": 0
+            },
+            {
+                "id": 28,
+                "name": "Phú hào điền trang",
+                "code": "phu_hao_dien_trang",
+                "icon": "https://file.gamee.vn/da0c470fe-rank_5.png",
+                "fromLevel": 16,
+                "toLevel": 21,
+                "requiredTotalExp": 167000,
+                "rankAwards": [
+                    {
+                        "id": 45,
+                        "awardType": {
+                            "id": 1,
+                            "name": "XP mỗi ngày"
+                        },
+                        "quantity": 100,
+                        "image": "https://file.gamee.vn/1733899230951_exp.f.svg"
+                    },
+                    {
+                        "id": 46,
+                        "awardType": {
+                            "id": 2,
+                            "name": "Ting Ting"
+                        },
+                        "quantity": 3000,
+                        "image": "https://file.gamee.vn/1734170262009_coin.png"
+                    },
+                    {
+                        "id": 47,
+                        "awardType": {
+                            "id": 3,
+                            "name": "Giảm giá"
+                        },
+                        "quantity": 10,
+                        "image": "https://file.gamee.vn/1733899238841_giamgia2.jpg"
+                    }
+                ],
+                "receivingStatus": 0
+            },
+            {
+                "id": 34,
+                "name": "Đệ nhất Đại Phú",
+                "code": "de_nhat_dai_phu",
+                "icon": "https://file.gamee.vn/546dc899d-rank_6.png",
+                "fromLevel": 22,
+                "toLevel": 26,
+                "requiredTotalExp": 1020000,
+                "rankAwards": [
+                    {
+                        "id": 49,
+                        "awardType": {
+                            "id": 2,
+                            "name": "Ting Ting"
+                        },
+                        "quantity": 5000,
+                        "image": "https://file.gamee.vn/1734170262009_coin.png"
+                    },
+                    {
+                        "id": 50,
+                        "awardType": {
+                            "id": 3,
+                            "name": "Giảm giá"
+                        },
+                        "quantity": 15,
+                        "image": "https://file.gamee.vn/1733899262205_giamgia2.jpg"
+                    },
+                    {
+                        "id": 56,
+                        "awardType": {
+                            "id": 1,
+                            "name": "XP mỗi ngày"
+                        },
+                        "quantity": 200,
+                        "image": "https://file.gamee.vn/1734497615448_exp.svg"
+                    }
+                ],
+                "receivingStatus": 0
+            }
+        ]
+    }
+}
